88e73d4fe3be22a4c97a8cfca21afb6bf896f7cf
filkodaria <filko_d@ukr.net>
2023-02-18 20:53:50 +0200

    script is edited after review

--- Task_1.sql
+++ Task_1.sql
@@ -2,21 +2,24 @@ CREATE DATABASE SQLEducation;
 
 CREATE TABLE EMPLOYEE
 (
-  EmpCode int,
-  EmpFName varchar(15),
-  EmpLName varchar(15),
-  Job varchar(45),
-  Manager char(4),
-  HireDate date,
-  Salary int,
-  Commission int,
-  DeptCode int
-)
+  EmpCode int IDENTITY(1,1) PRIMARY KEY,
+  EmpFName varchar(15) NOT NULL,
+  EmpLName varchar(15) NOT NULL,
+  Job varchar(45) NOT NULL,
+  Manager char(4) NOT NULL,
+  HireDate date NOT NULL,
+  Salary int NOT NULL,
+  Commission int NOT NULL,
+  DeptCode int NOT NULL
+);
 
 CREATE TABLE DEPARTMENT
 (
-  DeptCode int,
-  DeptName char(30),
-  Location varchar(33)
+  DeptCode int PRIMARY KEY,
+  DeptName char(30) NOT NULL,
+  Location varchar(33) NOT NULL
 )
+
+SELECT * FROM EMPLOYEE; 
+SELECT * FROM DEPARTMENT;
 
